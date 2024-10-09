f36efdb5ed7ce5899d42b894282e01a7464ba2cc
androidinflow <zakharname0111@gmail.com>
2024-10-10 00:29:24 +0200

    clash - qrcode domain simple back

--- src/routes/r/[shortCode]/+server.js
+++ src/routes/r/[shortCode]/+server.js
@@ -1,4 +1,4 @@
-import { error, redirect } from "@sveltejs/kit";
+import { error } from "@sveltejs/kit";
 
 export async function GET({ params, locals }) {
   const { shortCode } = params;
@@ -8,16 +8,23 @@ export async function GET({ params, locals }) {
       .collection("qrcodes")
       .getFirstListItem(`shortCode="${shortCode}"`);
 
-    if (!qrCode) {
+    if (qrCode) {
+      console.log("QR Code found :", qrCode.link);
+      // Ensure the link is a valid URL
+      let redirectUrl = qrCode.link;
+      if (
+        !redirectUrl.startsWith("http://") &&
+        !redirectUrl.startsWith("https://")
+      ) {
+        redirectUrl = "http://" + redirectUrl;
+      }
+      return new Response(null, {
+        status: 302,
+        headers: { Location: redirectUrl },
+      });
+    } else {
       throw error(404, "QR Code not found");
     }
-
-    const redirectUrl =
-      qrCode.link.startsWith("http://") || qrCode.link.startsWith("https://")
-        ? qrCode.link
-        : `http://${qrCode.link}`;
-
-    throw redirect(302, redirectUrl);
   } catch (err) {
     console.error("Error fetching QR code:", err);
     throw error(500, "Internal Server Error");
